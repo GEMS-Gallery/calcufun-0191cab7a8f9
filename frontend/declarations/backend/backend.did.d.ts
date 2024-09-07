dd0465c9742c87316c82428b192719c29d6a9cf1
sa-gems-github <sa-gems-github@dfinity.org>
2024-09-07 00:30:09 -0400

    0191cabe-e6e6-777b-8613-54409783e8e2

--- frontend/declarations/backend/backend.did.d.ts
+++ frontend/declarations/backend/backend.did.d.ts
@@ -2,11 +2,6 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface Stock { 'name' : string, 'price' : number, 'symbol' : string }
-export interface _SERVICE {
-  'getStocks' : ActorMethod<[], Array<Stock>>,
-  'trade' : ActorMethod<[string, bigint, boolean], string>,
-  'updateStockPrice' : ActorMethod<[string, number], undefined>,
-}
+export interface _SERVICE {}
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- frontend/declarations/backend/backend.did.js
+++ frontend/declarations/backend/backend.did.js
@@ -1,13 +1,2 @@
-export const idlFactory = ({ IDL }) => {
-  const Stock = IDL.Record({
-    'name' : IDL.Text,
-    'price' : IDL.Float64,
-    'symbol' : IDL.Text,
-  });
-  return IDL.Service({
-    'getStocks' : IDL.Func([], [IDL.Vec(Stock)], ['query']),
-    'trade' : IDL.Func([IDL.Text, IDL.Int, IDL.Bool], [IDL.Text], []),
-    'updateStockPrice' : IDL.Func([IDL.Text, IDL.Float64], [], []),
-  });
-};
+export const idlFactory = ({ IDL }) => { return IDL.Service({}); };
 export const init = ({ IDL }) => { return []; };
